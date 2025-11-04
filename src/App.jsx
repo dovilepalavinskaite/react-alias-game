2350e673d39769cb0e949fd2e76194341e4cbc78
dovilepalavinskaite <palavinskaite.dovile@gmail.com>
2025-11-04 14:51:21 +0200

    Initial commit: setup React app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh", justifyContent: "center", alignItems: "center" }}>
+      <h1>Alias Game</h1>
+      <p>My first React game 🚀</p>
+    </div>
+  );
+}
